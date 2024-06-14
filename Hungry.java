07245e4842d17b7bd773b81a7efe6c1ec0720c33
mukundaDev <94097024+mukundaDev@users.noreply.github.com>
2024-06-14 13:07:04 +0530

    added hungry prompt

--- Hungry.java
+++ Hungry.java
@@ -1,6 +1,9 @@
 public class Hungry {
     
     public static void  main(String[] args){
+        System.out.println("Eat samosa if your are Hungry");
         System.out.println("Hey, Im Hungry");
     }
+
+
 }
